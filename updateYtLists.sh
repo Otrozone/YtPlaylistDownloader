a57a109d932575fd876393c7137a282486ca55f2
otrozone <cek@seznam.cz>
2021-11-29 21:31:14 +0000

    Advance...

--- updateYtLists.sh
+++ updateYtLists.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 while IFS="" read -r p || [ -n "$p" ]
 do
-  printf '%s\n' "$p"
+  IFS='|' read var1 var2 <<< $p
+  mkdir -p "${SCRIPT_DIR}/${var1}"
+  cd "${SCRIPT_DIR}/${var1}"
+  ${SCRIPT_DIR}/updateYtList.sh $var2
 done < lists.txt
