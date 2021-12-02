85d60a674e7a21b961381e45879a42cf1d52368d
otrozone <cek@seznam.cz>
2021-12-02 09:39:07 +0000

    Switch from youtube-dl to yt-dlp

--- updateYtLists.sh
+++ updateYtLists.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+OUTPUT_PATH=/media/Media/Audio/Youtube
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 while IFS="" read -r p || [ -n "$p" ]
 do
   IFS='|' read var1 var2 <<< $p
-  mkdir -p "${SCRIPT_DIR}/${var1}"
-  cd "${SCRIPT_DIR}/${var1}"
+  mkdir -p "${OUTPUT_PATH}/${var1}"
+  cd "${OUTPUT_PATH}/${var1}"
   ${SCRIPT_DIR}/updateYtList.sh $var2
 done < lists.txt
