5ae8e0ae7f0c4c5baaaffd86133961508745d4db
otrozone <cek@seznam.cz>
2022-05-03 19:35:05 +0200

    Added id to the file name.

--- updateYtList.sh
+++ updateYtList.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # youtube-dl --download-archive downloaded.txt --no-post-overwrites -ciwx --audio-format mp3 -o "%(title)s.%(ext)s" $*
-yt-dlp --download-archive downloaded.txt --no-post-overwrites -ciwx --audio-format mp3 -o "%(title)s.%(ext)s" $*
+yt-dlp --download-archive downloaded.txt --no-post-overwrites -ciwx --audio-format mp3 -o "%(id)s-%(title)s.%(ext)s" $*
